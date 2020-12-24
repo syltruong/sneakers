0306d6ad646b21754138f4f81e613cb552e7ee8c
syltruong <syltruong@gmail.com>
2020-12-24 15:06:37 +0000

    correct for Path usage

--- data/download_data.py
+++ data/download_data.py
@@ -8,7 +8,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", "-o", type=str, required=True)
 
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     args = parser.parse_args()
 
     df = get_csv_data(count_limit=40000)
-    df.to_csv(args.output_dir / DATA_CSV_NAME , index=False)
+    df.to_csv(output_dir / DATA_CSV_NAME , index=False)
