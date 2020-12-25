c012f9a63d4d8adbdf94438188675eddf0392df6
syltruong <syltruong@gmail.com>
2020-12-25 03:40:49 +0000

    correction

--- data/download_images.py
+++ data/download_images.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", "-o", type=str, required=True)
     args = parser.parse_args()
     
-    directory = args.output_dir
+    directory = Path(args.output_dir)
     df = load_data_from_csv(directory / DATA_CSV_NAME)
 
     download_images(df, output_dir=directory / IMAGES_DIR_NAME)
