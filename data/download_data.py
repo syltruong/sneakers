9f7b508fb0091250a2088c1cc95c21f7065c19f1
syltruong <syltruong@gmail.com>
2020-12-25 03:37:02 +0000

    correction

--- data/download_data.py
+++ data/download_data.py
@@ -7,11 +7,11 @@ from data.utils import get_csv_data
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", "-o", type=str, required=True)
+    args = parser.parse_args()
 
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    args = parser.parse_args()
 
     df = get_csv_data(count_limit=40000)
     df.to_csv(output_dir / DATA_CSV_NAME , index=False)
